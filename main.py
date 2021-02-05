1e867700fed42adf486305d6efa1672add4c84c4
MakualDev <31779213+max2000505@users.noreply.github.com>
2021-02-05 21:14:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, make_response, request, session, copy_current_request_context
+import threading
+from server import app, sio
+
+from socket_processing import sp
+from request_processing import rp
+from modules import hf
+
+@app.route('/', methods = ['GET','POST'])
+def code():
+    return "Все работает"
+
+@app.route('/reg-code/', methods = ['POST'])
+def reg_code():
+    return rp.reg_code(request.data)
+
+@app.route('/registration/', methods = ['POST'])
+def reg_in():
+    return rp.reg_in(request.data)
+
+@app.route('/login/', methods = ['POST'])
+def log_in():
+    return rp.log_in(request.data)
+
+@app.route('/get_information/', methods = ['POST'])
+def get_information():
+    return rp.get_information(request.data)
+
+# @app.route('/user/<id>/')
+# def user_profile(id):
+#     return rp.user_profile(id)
+
+# @sio.on('smth')
+# def create_room(data):
+#     sp.do_smth(data)
+
+if __name__ == "__main__":
+    app.run()
